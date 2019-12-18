45e209de7090bcb1847127c3ac90d1b2835a7653
franklin-huang-mobiledgex <franklin.huang@mobiledgex.com>
2019-12-18 13:52:58 -0800

    handle different operating systems on cellular interface lookup

--- rest/MatchingEngineSDKRestLibrary/DistributedMatchEngine.cs
+++ rest/MatchingEngineSDKRestLibrary/DistributedMatchEngine.cs
@@ -507,6 +507,19 @@ namespace DistributedMatchEngine
         return findCloudletReply;
     }
 
+    // Override with specified dme host and port
+    public async Task<FindCloudletReply> RegisterAndFindCloudlet(string host, uint port, string carrierName, string developerName, string appName, string appVersion, string authToken, Loc loc)
+    {
+        // Register Client
+        RegisterClientRequest registerRequest = CreateRegisterClientRequest(carrierName, developerName, appName, appVersion, authToken);
+        await RegisterClient(host, port, registerRequest);
+        // Find Cloudlet 
+        FindCloudletRequest findCloudletRequest = CreateFindCloudletRequest(carrierName, developerName, appName, appVersion, loc);
+        FindCloudletReply findCloudletReply = await FindCloudlet(host, port, findCloudletRequest);
+
+        return findCloudletReply;
+    }
+
     public VerifyLocationRequest CreateVerifyLocationRequest(string carrierName, Loc loc)
     {
       if (sessionCookie == null)
