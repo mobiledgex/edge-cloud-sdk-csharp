3c30b638f2bfe18bf3eddd5c36362c142dda28ad
Garner Lee <1672745+lgarner@users.noreply.github.com>
2020-04-17 15:33:36 -0700

    EDGECLOUD-2408 Initial MelMessaging Interface for Unity.

---
+++ rest/MatchingEngineSDKRestLibrary/MelMessaging.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DistributedMatchEngine.Mel
+{
+  public interface MelMessagingInterface
+  {
+    public bool IsMelReady();
+    public bool IsMelEnabled();
+    public string GetUuid();
+    public string SetLocationToken(string location_token, string app_name);
+  }
+
+  class EmptyMelMessaging : MelMessagingInterface
+  {
+    public bool IsMelReady() { return false;  }
+    public bool IsMelEnabled() { return false; }
+    public string GetUuid() { return ""; }
+    public string SetLocationToken(string location_token, string app_name) { return ""; }
+  }
+}
