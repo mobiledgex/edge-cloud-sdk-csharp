fa0f71ecbc2d5bc0cd6f1e9c482dadaa52518fdd
A Hassan <ahmed.hassan@mobiledgex.com>
2020-05-05 11:46:39 -0700

    EDGECLOUD-2477 GetConnectionUtil

--- rest/MatchingEngineSDKRestLibrary/GetConnectionUtil.cs
+++ rest/MatchingEngineSDKRestLibrary/GetConnectionUtil.cs
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Copyright 2018-2020 MobiledgeX, Inc. All rights and licenses reserved.
  * MobiledgeX, Inc. 156 2nd Street #408, San Francisco, CA 94105
  *
@@ -24,7 +24,12 @@ using System.Collections.Generic;
 
 namespace DistributedMatchEngine
 { 
-
+  /// <summary>
+  /// Defines The Network Interface Name
+  /// <para>
+  /// (Network Interface is the point of interconnection between a device and (public or private) cloud)
+  /// </para>
+  /// </summary>
   public class NetworkInterfaceName
   {
     public string CELLULAR = null;
@@ -32,24 +37,47 @@ namespace DistributedMatchEngine
   }
 
   // Some known network interface profiles:
+  
+  /// <summary>
+  /// Defines the IOS Netwotk Interface
+  /// <para>
+  /// (Network Interface is the point of interconnection between a device and (public or private) cloud)
+  /// </para>
+  /// </summary>
   public class IOSNetworkInterfaceName : NetworkInterfaceName
   {
+    /// <summary>
+    /// Constructor for IOS Network Interface Cellular and Wifi
+    /// </summary>
     public IOSNetworkInterfaceName()
     {
       CELLULAR = "pdp_ip0";
       WIFI = "en0";
     }
   }
-
+  /// <summary>
+  /// Defines the Android Netwotk Interface
+  /// <para>
+  /// (Network Interface is the point of interconnection between a device and (public or private) cloud)
+  /// </para>
+  /// </summary>
   public class AndroidNetworkInterfaceName : NetworkInterfaceName
   {
+    /// <summary>
+    /// Constructor for Android Network Interface Cellular and Wifi
+    /// </summary>
     public AndroidNetworkInterfaceName()
     {
       CELLULAR = "radio0"; // rmnet_data0 for some older version of Android
       WIFI = "wlan0";
     }
   }
-
+  /// <summary>
+  /// Defines the MacOS Netwotk Interface
+  /// <para>
+  /// (Network Interface is the point of interconnection between a device and (public or private) cloud)
+  /// </para>
+  /// </summary>
   public class MacNetworkInterfaceName : NetworkInterfaceName
   {
     public MacNetworkInterfaceName()
@@ -61,6 +89,12 @@ namespace DistributedMatchEngine
 
   public partial class MatchingEngine
   {
+    /// <summary>
+    /// Function for Validating the Service Certificate
+    /// <para>Server Certificates are used for identifying Servers</para>
+    /// <para>Certificates are being used to serve the rationale of encrypting and decrypting the content.</para>
+    /// </summary>
+    /// <returns> Boolean value representing validation result </returns>
     private static bool ValidateServerCertificate(object sender,
       X509Certificate certificate,
       X509Chain chain, SslPolicyErrors sslPolicyErrors)
@@ -102,7 +136,15 @@ namespace DistributedMatchEngine
       }
       return true;
     }
-
+    
+    /// <summary>
+    /// Validate Public Port ,Used in MatchingEngine.CreateURL
+    /// </summary>
+    /// <param name="findCloudletReply"> FindCloudletReply Object</param>
+    /// <param name="appPort">AppPort Object</param>
+    /// <param name="proto">Protocol (HTTP,TCP,UDP)</param>
+    /// <param name="portNum">Port Number</param>
+    /// <returns>Returns the AppPort </returns>
     public static AppPort ValidatePublicPort(FindCloudletReply findCloudletReply, AppPort appPort, LProto proto, int portNum)
     {
       AppPort found = null;
@@ -121,7 +163,13 @@ namespace DistributedMatchEngine
       return found;
     }
 
-    // Create a L7Path URL from an app port:
+    /// <summary>
+    ///  Create a L7Path URL using findCloudletReply, AppPort Object and desired port number, (L7 is the Application layer)
+    /// </summary>
+    /// <param name="findCloudletReply"> Reply from MatchingEngine.FindCloudlet</param>
+    /// <param name="appPort">App Port Object</param>
+    /// <param name="portNum">Desired Port Number</param>
+    /// <returns> Returns url if port was found and null if failed</returns>
     public static string CreateUrl(FindCloudletReply findCloudletReply, AppPort appPort, int portNum)
     {
       int aPortNum = portNum <= 0 ? appPort.public_port : portNum;
@@ -139,8 +187,13 @@ namespace DistributedMatchEngine
 
       return url;
     }
-
-    // Checks if the specified port is within the range of public_port and end_port
+    
+    /// <summary>
+    /// Checks if the specified port is within the range of public_port and end_port
+    /// </summary>
+    /// <param name="appPort">AppPort Object</param>
+    /// <param name="port"> Desired Port Number</param>
+    /// <returns>boolean value</returns>
     private static bool IsInPortRange(AppPort appPort, int port)
     {
       // Checks if range exists -> if not, check if specified port equals public port
@@ -150,8 +203,12 @@ namespace DistributedMatchEngine
       }
       return (port >= appPort.public_port && port <= appPort.end_port);
     }
-
-    // Gets IP Address of the specified network interface
+    
+    /// <summary>
+    /// Gets IP Address of the specified network interface
+    /// </summary>
+    /// <param name="port"> port number </param>
+    /// <returns>IPEndPoint Object (IPEndPoint class is part of System.Net.EndPoint)</returns>
     private IPEndPoint GetLocalIP(int port = 0)
     {
       if (netInterface == null)
@@ -179,7 +236,13 @@ namespace DistributedMatchEngine
       IPEndPoint localEndPoint = new IPEndPoint(localIP, port);
       return localEndPoint;
     }
-
+    
+    /// <summary>
+    /// Gets AppPorts By Protocol using FindCloudletReply and a specific Protocol
+    /// </summary>
+    /// <param name="reply"> FindCloudletReply Object</param>
+    /// <param name="proto"> Network Protocol (HTTP,UDP,TCP) </param>
+    /// <returns> Dictionary of Application ports {Internal port, Related AppPort Object}</returns>
     public Dictionary<int, AppPort> GetAppPortsByProtocol(FindCloudletReply reply, LProto proto)
     {
       Dictionary<int, AppPort> appPortsByProtocol = new Dictionary<int, AppPort>();
@@ -193,7 +256,12 @@ namespace DistributedMatchEngine
       }
       return appPortsByProtocol;
     }
-
+    
+    /// <summary>
+    /// Gets AppPorts for TCP Protocol using FindCloudletReply 
+    /// </summary>
+    /// <param name="reply"> FindCloudletReply Object </param>
+    /// <returns> Dictionary of Application ports {Internal port number, Related AppPort Object}</returns>
     public Dictionary<int, AppPort> GetTCPAppPorts(FindCloudletReply reply)
     {
       Dictionary<int, AppPort> tcpAppPorts = new Dictionary<int, AppPort>();
@@ -207,7 +275,11 @@ namespace DistributedMatchEngine
       }
       return tcpAppPorts;
     }
-
+    /// <summary>
+    /// Gets AppPorts for UDP Protocol using FindCloudletReply 
+    /// </summary>
+    /// <param name="reply"> FindCloudletReply Object </param>
+    /// <returns> Dictionary of Application ports {Internal port number, Related AppPort Object}</returns>
     public Dictionary<int, AppPort> GetUDPAppPorts(FindCloudletReply reply)
     {
       Dictionary<int, AppPort> udpAppPorts = new Dictionary<int, AppPort>();
@@ -221,7 +293,11 @@ namespace DistributedMatchEngine
       }
       return udpAppPorts;
     }
-
+    /// <summary>
+    /// Gets AppPorts for HTTP Protocol using FindCloudletReply 
+    /// </summary>
+    /// <param name="reply"> FindCloudletReply Object </param>
+    /// <returns> Dictionary of Application ports {Internal port number, Related AppPort Object}</returns>
     public Dictionary<int, AppPort> GetHTTPAppPorts(FindCloudletReply reply)
     {
       Dictionary<int, AppPort> httpAppPorts = new Dictionary<int, AppPort>();
