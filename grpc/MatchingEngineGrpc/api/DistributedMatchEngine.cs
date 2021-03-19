a5a646ea576824208d62c7cd068d0c4f5aef832d
Garner Lee <1672745+lgarner@users.noreply.github.com>
2021-03-19 15:29:14 -0700

    Tests for latency running. Aded a Teardown call to test Dispose. Documentaiton comments added.

--- grpc/MatchingEngineGrpc/api/DistributedMatchEngine.cs
+++ grpc/MatchingEngineGrpc/api/DistributedMatchEngine.cs
@@ -1586,35 +1586,43 @@ namespace DistributedMatchEngine
     // Compiler generated.
     protected virtual void Dispose(bool disposing)
     {
-      if (EdgeEventsConnection == null || !EdgeEventsConnection.IsShutdown())
+      if (disposedValue)
       {
-        if (disposing)
-        {
-          // TODO: dispose managed state (managed objects)
-        }
-
-        // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-        // TODO: set large fields to null
-        if (EdgeEventsConnection == null || !EdgeEventsConnection.IsShutdown())
-        {
-          EdgeEventsConnection.Dispose();
-        }
-        authToken = null;
-        sessionCookie = null;
-        LastFindCloudletReply = null;
-        LastRegisterClientRequest = null;
-        // FIXME: EventBusReciever( /* Terminate Event to consumers /* )
-        EventBusReciever = null;
-        disposedValue = true;
+        return;
       }
+
+      if (disposing)
+      {
+        // dispose managed state (managed objects)
+        if (EdgeEventsConnection != null || !EdgeEventsConnection.IsShutdown())
+        {
+          EdgeEventsConnection.Close();
+        }
+        EventBusReciever = null;
+
+      }
+
+      // free unmanaged resources (unmanaged objects) and override finalizer
+      // set large fields to null
+      if (httpClient != null)
+      {
+        httpClient.Dispose();
+      }
+      httpClient = null;
+      EdgeEventsConnection = null;
+      authToken = null;
+      sessionCookie = null;
+      LastFindCloudletReply = null;
+      LastRegisterClientRequest = null;
+      disposedValue = true;
     }
 
-    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-    // ~MatchingEngine()
-    // {
-    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-    //     Dispose(disposing: false);
-    // }
+    // override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~MatchingEngine()
+    {
+       // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+       Dispose(disposing: false);
+    }
 
     public void Dispose()
     {
