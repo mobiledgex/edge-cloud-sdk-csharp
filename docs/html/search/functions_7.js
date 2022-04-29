66e8c5d760c6d3665bf8ca6347bcfc6e209be9ee
gainsley <gainsley@users.noreply.github.com>
2022-04-29 23:47:57 +0000

    Docs Automated Updates

--- docs/html/search/functions_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['nettest_0',['NetTest',['../class_distributed_match_engine_1_1_performance_metrics_1_1_net_test.html#a7337e9ad57657fa0c395fff978cf143c',1,'DistributedMatchEngine::PerformanceMetrics::NetTest']]]
-];
--- docs/html/search/namespaces_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['distributedmatchengine_0',['DistributedMatchEngine',['../namespace_distributed_match_engine.html',1,'']]],
-  ['performancemetrics_1',['PerformanceMetrics',['../namespace_distributed_match_engine_1_1_performance_metrics.html',1,'DistributedMatchEngine']]]
-];
